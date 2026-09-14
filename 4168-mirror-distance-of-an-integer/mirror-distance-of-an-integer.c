d97683111a7e14bf1019738e17b0e9298c7a6365
Komal Gaur <gaurk1953@gmail.com>
2026-09-14 10:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (32.76%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.c
@@ -0,0 +1,12 @@
+int rev(int n) {
+    int a = 0;
+    while(n > 0) {
+        a = a * 10 + (n % 10);
+        n /= 10;
+    }
+    return a;
+}
+int mirrorDistance(int n) {
+    int m = rev(n);
+    return abs(m - n);
+}
